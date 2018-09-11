dc151419968ea6252361c6d9f6ddd2ff277f5f8d
Dimgba Kalu <dkdimgba@yahoo.com>
2018-09-11 14:18:02 +0100

    Update SendMessage.php

--- src/SendMessage.php
+++ src/SendMessage.php
@@ -3,56 +3,42 @@
 namespace App;
 use App\Config;
 class SendMessage {
-
 	public $userID;
 	public $accessToken;
-  public $input;
-
+	public $input;
+	
 	function __construct($input) {
-
-    $this->userID = $input['entry'][0]['messaging'][0]['sender']['id'];
-    $this->accessToken = Config::getAccessToken();
-    $this->input = $input;
-
+		$this->userID = $input['entry'][0]['messaging'][0]['sender']['id'];
+		$this->accessToken = Config::getAccessToken();
+		$this->input = $input;
 	}
 
 
-  public function text($text) {
+  	public function text($text) {
+		$userID = $this->userID;
+		
+		$response = [
+			'recipient' => [ 'id' => $userID ],
+			'message' => [ 'text' => $text ]
+		];
 
-	  $userID = $this->userID;
-	   
-	  $response = [
-
-	     'recipient' => [ 'id' => $userID ],
-
-	     'message' => [ 'text' => $text ]
-
-	  ];
-
-    $this->curlResponse($response);
-
-   
-  }
+    		$this->curlResponse($response);
+	}
 	
-     public function curlResponse($response) {
-
-      $accessToken = $this->accessToken;
-	    $input=  $this->input;
+     	public function curlResponse($response) {
+		$accessToken = $this->accessToken;
+		$input=  $this->input;
 	
-	    $curl = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-	    curl_setopt($curl, CURLOPT_POST, 1);
-	    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($response));
-	    curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+	    	$curl = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+	    	curl_setopt($curl, CURLOPT_POST, 1);
+	    	curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($response));
+	    	curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 
-      if(!empty($input)) {
-	        
-          curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	        $result = curl_exec($curl);
-      }
+      		if(!empty($input)) {
+	        	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	        	$result = curl_exec($curl);
+      		}
 
-      curl_close($curl); 
-
-	  }
-
-
+      		curl_close($curl); 
+	}
 }
