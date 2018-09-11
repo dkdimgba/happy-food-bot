bc7df13f9a2967a1168e5ccdae4baf9bbc5d21db
Dimgba Kalu <dkdimgba@yahoo.com>
2018-09-11 14:07:55 +0100

    Update VerifyBot.php

--- src/VerifyBot.php
+++ src/VerifyBot.php
@@ -1,31 +1,15 @@
 <?php
-
 namespace App;
 use App\Config;
 //verifies bot credentials with that of facebook
 class VerifyBot
 {
-
     function __construct()
     {
-
          $verifyToken = Config::getVerifyToken();
-
-		 if ($_REQUEST['hub_verify_token'] === $verifyToken)
-		  {
-
-				  echo $_REQUEST['hub_challenge'];
-
-
-
-		  } 
- 
-
+	 if ($_REQUEST['hub_verify_token'] === $verifyToken)
+	 {
+		 echo $_REQUEST['hub_challenge'];
+	 } 
     }
-
-    
-    
-
-
-
 }
