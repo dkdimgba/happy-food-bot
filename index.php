0ce03ec82687733268509e53338e19c8d0bbaffc
Dimgba Kalu <dkdimgba@yahoo.com>
2018-09-11 14:00:15 +0100

    Update index.php
    Changes the response message to fit bot function

--- index.php
+++ index.php
@@ -4,23 +4,12 @@ use  App\VerifyBot;
 use App\ReceivedMessage;
 use App\SendMessage;
 
-
 $verifyBot = new VerifyBot();
-
- 
 $input = json_decode(file_get_contents('php://input'), true);
-
 
 $receivedMessage = new ReceivedMessage($input);
 $sendMessage = new SendMessage($input);
 
- 
 if(!empty($receivedMessage->textMessage)) {   
-     $sendMessage->text("hello coder");
-    
+     $sendMessage->text("Hello, welcome to Happy Food. How can we be of help to you?");
 }
- 
-
-
-
- 
